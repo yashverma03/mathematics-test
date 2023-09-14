e3cb1d233112dd594bcf52cdd7721379946e1823
yashverma03 <vermayash2003@gmail.com>
2023-09-14 09:27:18 +0530

    added config

---
+++ src/config/api.js
@@ -0,0 +1,3 @@
+const apiUrl = `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails`;
+
+export default apiUrl;
---
+++ src/config/data.js
@@ -0,0 +1,12 @@
+const questionId = {
+  question1: 'AreaUnderTheCurve_21',
+  question2: 'BinomialTheorem_13',
+  question3: 'BinomialTheorem_24',
+  question4: 'AreaUnderTheCurve_15',
+  question5: 'AreaUnderTheCurve_2',
+  question6: 'BinomialTheorem_3',
+  question7: 'BinomialTheorem_4',
+  question8: 'AreaUnderTheCurve_5',
+};
+
+export default questionId;
